1f62630bba250d2156c2f520f3bacf0ece34caa7
TOMTOM <36344451+illusiongaming@users.noreply.github.com>
2018-10-15 15:28:00 +0200

    Update secondjob.sql

--- esx_secondjob/secondjob.sql
+++ esx_secondjob/secondjob.sql
@@ -5,6 +5,13 @@ ALTER TABLE `users`
 	ADD COLUMN `job_grade2` INT NULL DEFAULT 0 AFTER `job2`
 ;
 
+----insert in jobs
+INSERT INTO `jobs` VALUES ('unemployed2','Arbeitslos');
+
+
+---just with your sql you are not following steps as it is described 
+
+---- Why create a job_grade table ?
 CREATE TABLE `jobs2` (
 	`name` varchar(50) NOT NULL,
 	`label` varchar(255) DEFAULT NULL,
@@ -12,9 +19,9 @@ CREATE TABLE `jobs2` (
 	PRIMARY KEY (`name`)
 );
 
-INSERT INTO `jobs2` VALUES ('unemployed2','Arbeitslos');
 
 
+---- Why create a job_grade table delete that?
 CREATE TABLE `job_grades2` (
 	`id` int(11) NOT NULL AUTO_INCREMENT,
 	`job_name` varchar(255) DEFAULT NULL,
@@ -27,7 +34,7 @@ CREATE TABLE `job_grades2` (
 
 	PRIMARY KEY (`id`)
 );
-
+---- Why create a job_grade table delete that?
 INSERT INTO `job_grades2` VALUES (1,'unemployed2',0,'rsa','Los Angeles',5,'{}','{}')
 ;
 
